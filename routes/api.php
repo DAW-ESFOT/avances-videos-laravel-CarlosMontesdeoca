ff2dc6b2960e903f809d8e5b4acbd19e6746e724
CarlosMontesdeoca <mateo.game@hotmail.com>
2020-12-16 10:21:32 -0500

    #9 crear rutas rutas para articles comprobacion con postman - Get - Post - Puy - Delete

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
-
+// https://app.getpostman.com/join-team?invite_code=09e540f8950c8f4f45d79d8c533171d9
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+// OBtener la lista
+Route::get('articles', function(){
+    return Article::all();
+});
+// obtener un solo registro
+Route::get('articles/{id}', function($id){
+    return Article::find($id);
+});
+// Crear un solo registro
+Route::post('articles', function(Request $request){
+    return Article::create($request->all());
+});
+// modificar un solo registro
+Route::put('articles/{id}', function(Request $request, $id){
+    $article = Article::findOrFail($id);
+    $article->update($request->all());
+    return $article;
+});
+// borrar un registro
+Route::delete('articles/{id}', function($id){
+    Article::find($id)->delete();
+    return 204;
 });
