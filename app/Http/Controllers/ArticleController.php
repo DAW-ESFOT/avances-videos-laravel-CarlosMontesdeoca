a1763e247ca03fb5336277c5db9b52425f02f902
CarlosMontesdeoca <mateo.game@hotmail.com>
2021-01-07 14:33:05 -0500

    #·2 recouse article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Article;
+use App\Http\Resources\Article as ArticleResuce;
+use App\Http\Resources\ArticleCollection;
 
 class ArticleController extends Controller
 {
     public function index(){
-        return Article::all();
+        return new ArticleCollection(Article::paginate(10));
     }
     public function show( Article $article){
-        return $article;
+        return response()->json(new ArticleResuce($article),200);
     }
     public function store(Request $request){
         $article = Article::create($request->all());
